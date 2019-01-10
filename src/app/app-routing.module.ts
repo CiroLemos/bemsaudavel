9ae74082e5f9d0673bc88a50d2424a894ca72fe5
Ciro Lemos <ciro.lemos01@gmail.com>
2019-01-10 21:25:08 -0200

    router home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'login', loadChildren: './login/login-user.module#LoginUserModule'}
+  {path:'login', loadChildren: './login/login-user.module#LoginUserModule'},
+  {path:'', loadChildren: './home/pagina-inicial/pagina-inicial.module#PaginaInicialModule'}
 ];
 
 @NgModule({
--- src/app/home/pagina-inicial/pagina-inicial.module.ts
+++ src/app/home/pagina-inicial/pagina-inicial.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
 import { PaginaInicialComponent } from './pagina-inicial.component';
 
+const routes: Routes = [
+  {path: '', component: PaginaInicialComponent}
+];
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
+  exports: [RouterModule],
   declarations: [PaginaInicialComponent]
 })
 export class PaginaInicialModule { }
